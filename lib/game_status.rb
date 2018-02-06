7bd9a35736e7f1b25ecb218a2782bc6dc423b56d
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-06 23:58:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,7 +17,7 @@ def position_taken?(board, index)
 end
 
 def won?(board)
-  if board != [" ", " ", " ", " ", " ", " ", " ", " ", " "] && draw?(board) == false
+  if board != [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
    WIN_COMBINATIONS.each do |combo|
      if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
        return combo
