a7235b1977ef6565d577027da97b55f6d3cbe995
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-06 23:47:45 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,7 +27,7 @@ def won?(board)
    WIN_COMBINATIONS.each do |combo|
      if board[combo[0]] == "X" && board[combo[1]] == "X" && board[combo[2]] == "X"
        return combo
-     elsif (board[combo[0]] != "X" && board[combo[1]] != "X" && board[combo[2]] != "X") && board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
+     elsif board[combo[0]] == "O" && board[combo[1]] == "O" && board[combo[2]] == "O"
        return combo
      end
    end
