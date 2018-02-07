5829c7c4eee8b603dc3b7242bb212d5ab3867255
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-07 00:01:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -35,7 +35,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == false && full?(board) == false
+  if !won?(board) && full?(board)
     return false
   elsif won?(board) != false
     return false
